e543a5498c6fd33c2523ea73ca2406d67b823187
Priya Dikey <priyadike@users.noreply.huggingface.co>
2026-03-31 14:37:09 +0000

    Update agent.py

--- agent.py
+++ agent.py
@@ -11,7 +11,7 @@ from langchain_community.document_loaders import ArxivLoader
 from langchain_community.vectorstores import SupabaseVectorStore
 from langchain_core.messages import SystemMessage, HumanMessage
 from langchain_community.retrievers import WikipediaRetriever
-from langchain.tools import create_retriever_tool
+from langchain_community.tools.retriever import create_retriever_tool
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain_community.llms import YandexGPT
 from langchain_core.tools import tool
