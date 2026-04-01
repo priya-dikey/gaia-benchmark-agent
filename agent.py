80e135116cc28d609b2217a13bd4f5615b166766
Priya Dikey <priyadike@users.noreply.huggingface.co>
2026-04-01 14:45:59 +0000

    Update agent.py

--- agent.py
+++ agent.py
@@ -156,7 +156,7 @@ def build_graph():
     # test
 if __name__ == "__main__":
     question = "When was a picture of St. Thomas Aquinas first added to the Wikipedia page on the Principle of double effect?"
-    graph = build_graph(provider="groq")
+    graph = build_graph()
     messages = [HumanMessage(content=question)]
     messages = graph.invoke({"messages": messages})
     for m in messages["messages"]:
