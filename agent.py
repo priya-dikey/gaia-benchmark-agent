1f150f7c2e667f1281c7a82cdae5abae047e7984
Priya Dikey <priyadike@users.noreply.huggingface.co>
2026-04-01 09:35:44 +0000

    Update agent.py

--- agent.py
+++ agent.py
@@ -119,15 +119,12 @@ tools = [
 ]
 
 def build_graph():
-    llm = HuggingFaceEndpoint( 
-        repo_id = "Qwen/Qwen2.5-Coder-32B-Instruct",
-        #repo_id="HuggingFaceH4/zephyr-7b-beta",
-        #task="conversational",
-      #  temperature=0.1,
-      #  max_new_tokens=500, 
-      #  huggingfacehub_api_token=os.environ.get("HUGGINGFACEHUB_API_TOKEN") 
-   # )
-
+     llm = ChatHuggingFace(
+        llm=HuggingFaceEndpoint(
+            repo_id = "Qwen/Qwen2.5-Coder-32B-Instruct"
+        ),
+    )
+    
     #llm_with_tools = llm.bind_tools(tools)
 
     def assistant(state: MessagesState):
