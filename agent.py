1c510f7059f31c9fda9b78de2202e5dfb8c95bb9
Priya Dikey <priyadike@users.noreply.huggingface.co>
2026-04-01 07:24:09 +0000

    Update agent.py

--- agent.py
+++ agent.py
@@ -119,7 +119,8 @@ tools = [
 
 def build_graph():
     llm = HuggingFaceEndpoint( 
-        repo_id="mistralai/Mistral-7B-Instruct-v0.2",
+        repo_id="HuggingFaceH4/zephyr-7b-beta",
+        task="text-generation",
         temperature=0.1,
         max_new_tokens=500, 
         huggingfacehub_api_token=os.environ.get("HUGGINGFACEHUB_API_TOKEN") 
